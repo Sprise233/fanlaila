6abf335884ef2237746c5246563a6b8b35766cdf
Sprise233 <2920756809@qq.com>
2025-05-20 21:26:50 +0800

    梦开始的地方

---
+++ FanLaiLa/src/main/java/com/github/sprise233/fanlaila/FanLaiLaApplication.java
@@ -0,0 +1,13 @@
+package com.github.sprise233.fanlaila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanLaiLaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanLaiLaApplication.class, args);
+    }
+
+}
---
+++ FanLaiLa/src/test/java/com/github/sprise233/fanlaila/FanLaiLaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sprise233.fanlaila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanLaiLaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
